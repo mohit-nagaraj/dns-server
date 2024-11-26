089817edc6c6c80dd7b798323fef757902a32d9f
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-11-26 22:02:23 +0530

    test question

--- app/main.go
+++ app/main.go
@@ -1,19 +1,14 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
 )
 
-// Ensures gofmt doesn't remove the "net" import in stage 1 (feel free to remove this!)
-var _ = net.ListenUDP
-
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
 	if err != nil {
 		fmt.Println("Failed to resolve UDP address:", err)
@@ -39,9 +34,25 @@ func main() {
 		receivedData := string(buf[:size])
 		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
 
-		response := make([]byte, 12)
+		response := make([]byte, 28) // 12 bytes for header + 16 bytes for question
 		copy(response, buf[:12])
 		response[2] = flipIndicator(response[2])
+
+		// Set the ID to 1234
+		binary.BigEndian.PutUint16(response[0:2], 1234)
+
+		// Set QDCOUNT to 1
+		binary.BigEndian.PutUint16(response[4:6], 1)
+
+		// Encode the domain name "codecrafters.io"
+		domainName := []byte{0x0c, 'c', 'o', 'd', 'e', 'c', 'r', 'a', 'f', 't', 'e', 'r', 's', 0x02, 'i', 'o', 0x00}
+		copy(response[12:], domainName)
+
+		// Set Type to 1 (A record)
+		binary.BigEndian.PutUint16(response[28:30], 1)
+
+		// Set Class to 1 (IN)
+		binary.BigEndian.PutUint16(response[30:32], 1)
 
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
