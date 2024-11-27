4a91782c72087bac63c8882bfc8fed6bfbe4a1bc
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-11-27 13:48:13 +0530

    answer section

--- app/main.go
+++ app/main.go
@@ -1,60 +1,54 @@
 package main
 
-import (
-	"encoding/binary"
-	"fmt"
-	"log"
-	"net"
-	"strings"
-)
+import "encoding/binary"
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-	udpAddr, err := net.ResolveUDPAddr("udp", "localhost:2053")
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	udpConn, err := net.ListenUDP("udp", udpAddr)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	defer udpConn.Close()
-	buf := make([]byte, 512)
-	for {
-		size, source, err := udpConn.ReadFromUDP(buf)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		message := string(buf[:size])
-		fmt.Println(message)
-		fmt.Println(buf[:2])
-		response := []byte{}
-		response = append(response, buf[:2]...)
-		response = append(response, 0b10000000)
-		response = append(response, 0b00000000)
-		response = binary.BigEndian.AppendUint16(response, uint16(1)) // QDCOUNT
-		response = binary.BigEndian.AppendUint16(response, 0x0000)    // ANCOUNT
-		response = binary.BigEndian.AppendUint16(response, 0x0000)    // NSCOUNT
-		response = binary.BigEndian.AppendUint16(response, 0x0000)    // ARCOUNT
-		response = append(response, encodeDomain("codecrafters.io")...)
-		response = binary.BigEndian.AppendUint16(response, uint16(1))
-		response = binary.BigEndian.AppendUint16(response, uint16(1))
-		if _, err := udpConn.WriteToUDP(response, source); err != nil {
-			log.Fatal(err.Error())
-		}
+type Message struct {
+	Header   []byte
+	Question []byte
+	//Answer
+	Answer []byte
+	//Authority
+	//Additional
+}
+
+func NewMessage() *Message {
+	return &Message{
+		Header:   make([]byte, 12),
+		Question: []byte{},
+		Answer:   []byte{},
 	}
 }
-func encodeDomain(domain string) []byte {
-	encodes := []byte{}
-	for _, seg := range strings.Split(domain, ".") {
-		n := len(seg)
-		encodes = append(encodes, byte(n))
-		encodes = append(encodes, []byte(seg)...)
-	}
-	return append(encodes, 0x00)
+func (m *Message) SetHeader() {
+	binary.BigEndian.PutUint16((*m).Header[0:2], 1234)
+	binary.BigEndian.PutUint16((*m).Header[2:4], combineFlags(1, 0, 0, 0, 0, 0, 0, 0))
+	binary.BigEndian.PutUint16((*m).Header[4:6], 1)
+	binary.BigEndian.PutUint16((*m).Header[6:8], 0)
+	binary.BigEndian.PutUint16((*m).Header[6:8], 1)
+	binary.BigEndian.PutUint16((*m).Header[8:10], 0)
+	binary.BigEndian.PutUint16((*m).Header[10:12], 0)
 }
-func bigendEncode(v int) []byte {
-	encodes := []byte{}
-	return binary.BigEndian.AppendUint16(encodes, uint16(v))
+func (m *Message) SetQuestion() {
+	(*m).Question = []byte("\x0ccodecrafters\x02io\x00")
+	(*m).Question = binary.BigEndian.AppendUint16((*m).Question, 1)
+	(*m).Question = binary.BigEndian.AppendUint16((*m).Question, 1)
+}
+func (m *Message) SetAnswer() {
+	answer := []byte{}
+	answer = append(answer, []byte("\x0ccodecrafters\x02io\x00")...)
+	answer = binary.BigEndian.AppendUint16(answer, 1)
+	answer = binary.BigEndian.AppendUint16(answer, 1)
+	answer = binary.BigEndian.AppendUint32(answer, 60)
+	answer = binary.BigEndian.AppendUint16(answer, 4)
+	answer = binary.BigEndian.AppendUint32(answer, binary.BigEndian.Uint32([]byte("\x08\x08\x08\x08")))
+	(*m).Answer = answer
+}
+func PrepareMessage() *Message {
+	message := NewMessage()
+	message.SetHeader()
+	message.SetQuestion()
+	message.SetAnswer()
+	return message
+}
+func combineFlags(qr, opcode, aa, tc, rd, ra, z, rcode uint) uint16 {
+	return uint16(qr<<15 | opcode<<11 | aa<<10 | tc<<9 | rd<<8 | ra<<7 | z<<4 | rcode)
 }
