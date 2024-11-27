a89c95eba7e49a3b809f9016efd328dc04f7ee36
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-11-27 13:42:41 +0530

    test working question section?

--- app/main.go
+++ app/main.go
@@ -3,64 +3,59 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"log"
 	"net"
+	"strings"
 )
 
 func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	udpAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:2053")
+	udpAddr, err := net.ResolveUDPAddr("udp", "localhost:2053")
 	if err != nil {
-		fmt.Println("Failed to resolve UDP address:", err)
-		return
+		log.Fatal(err.Error())
 	}
-
 	udpConn, err := net.ListenUDP("udp", udpAddr)
 	if err != nil {
-		fmt.Println("Failed to bind to address:", err)
-		return
+		log.Fatal(err.Error())
 	}
 	defer udpConn.Close()
-
 	buf := make([]byte, 512)
-
 	for {
 		size, source, err := udpConn.ReadFromUDP(buf)
 		if err != nil {
-			fmt.Println("Error receiving data:", err)
-			break
+			log.Fatal(err.Error())
 		}
-
-		receivedData := string(buf[:size])
-		fmt.Printf("Received %d bytes from %s: %s\n", size, source, receivedData)
-
-		response := make([]byte, 28) // 12 bytes for header + 16 bytes for question
-		copy(response, buf[:12])
-		response[2] = flipIndicator(response[2])
-
-		// Set the ID to 1234
-		binary.BigEndian.PutUint16(response[0:2], 1234)
-
-		// Set QDCOUNT to 1
-		binary.BigEndian.PutUint16(response[4:6], 1)
-
-		// Encode the domain name "codecrafters.io"
-		domainName := []byte{0x0c, 'c', 'o', 'd', 'e', 'c', 'r', 'a', 'f', 't', 'e', 'r', 's', 0x02, 'i', 'o', 0x00}
-		copy(response[12:], domainName)
-
-		// Set Type to 1 (A record)
-		binary.BigEndian.PutUint16(response[28:30], 1)
-
-		// Set Class to 1 (IN)
-		binary.BigEndian.PutUint16(response[30:32], 1)
-
-		_, err = udpConn.WriteToUDP(response, source)
-		if err != nil {
-			fmt.Println("Failed to send response:", err)
+		message := string(buf[:size])
+		fmt.Println(message)
+		fmt.Println(buf[:2])
+		response := []byte{}
+		response = append(response, buf[:2]...)
+		response = append(response, 0b10000000)
+		response = append(response, 0b00000000)
+		response = append(response, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00)
+		response = binary.BigEndian.AppendUint16(response, uint16(1)) // QDCOUNT
+		response = binary.BigEndian.AppendUint16(response, 0x0000)    // ANCOUNT
+		response = binary.BigEndian.AppendUint16(response, 0x0000)    // NSCOUNT
+		response = binary.BigEndian.AppendUint16(response, 0x0000)    // ARCOUNT
+		response = append(response, encodeDomain("codecrafters.io")...)
+		response = binary.BigEndian.AppendUint16(response, uint16(1))
+		response = binary.BigEndian.AppendUint16(response, uint16(1))
+		if _, err := udpConn.WriteToUDP(response, source); err != nil {
+			log.Fatal(err.Error())
 		}
 	}
 }
-
-func flipIndicator(b byte) byte {
-	return b | 0b10000000
+func encodeDomain(domain string) []byte {
+	encodes := []byte{}
+	for _, seg := range strings.Split(domain, ".") {
+		n := len(seg)
+		encodes = append(encodes, byte(n))
+		encodes = append(encodes, []byte(seg)...)
+	}
+	return append(encodes, 0x00)
+}
+func bigendEncode(v int) []byte {
+	encodes := []byte{}
+	return binary.BigEndian.AppendUint16(encodes, uint16(v))
 }
